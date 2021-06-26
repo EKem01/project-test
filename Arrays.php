e3b4e21f9ffef0ebc540eec8daa24e9f7f27d066
Ekem-tech <emmakem4real@gmail.com>
2021-06-26 18:01:42 +0100

    first commit

---
+++ Arrays.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php 
+$numbers = range("1", "10");
+
+foreach($numbers as $numbers){
+    echo $numbers. "<br>";
+}
+
+?>
+
+</body>
+</html>
---
+++ Functions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php 
+    function sum($num1, $num2){
+        return $num1 + $num2. "<br>";
+    }
+
+    echo sum(10, 50);
+    echo sum(10, 35.5);
+    echo sum(20, 60);
+    echo sum(40, 60);
+    
+
+?>
+
+</body>
+</html>
